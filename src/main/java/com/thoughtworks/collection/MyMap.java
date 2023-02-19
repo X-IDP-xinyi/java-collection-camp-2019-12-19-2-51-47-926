ad8a670cbfb4d28e1fd7017d5d08fee942acd15e
Andrea <Andrea2000728>
2023-02-19 22:23:39 +0800

    feat: implement the MyMap class and pass the test

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -1,38 +1,61 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
     List<Integer> array;
-    private String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    private final String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    private List<String> letterList = Arrays.asList(letters);
+    private final List<String> letterList = Arrays.asList(letters);
 
     public MyMap(List<Integer> array) {
         this.array = array;
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream().map(value -> value * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> newList = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            newList.add(letterList.get(i));
+        }
+        return newList;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        ArrayList<String> newList = new ArrayList<>();
+        for (Integer n : array) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            while (n > 0) {
+                if (n % 26 == 0) {
+                    temp.add(26);
+                    n = n / 26 - 1;
+                    continue;
+                }
+                temp.add(n % 26);
+                n = n / 26;
+            }
+            Collections.reverse(temp);
+            newList.add(temp.stream().map(x -> letters[x - 1]).collect(Collectors.joining()));
+        }
+        return newList;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        array.sort(Comparator.reverseOrder());
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        array.sort(Comparator.naturalOrder());
+        return array;
     }
 }
